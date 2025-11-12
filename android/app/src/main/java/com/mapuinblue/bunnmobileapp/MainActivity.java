42be2d1eaa912e60eadf0a24e1ca7aa11c756feb
Mapu <mapublume@gmail.com>
2025-11-12 15:07:44 -0500

    Android config

---
+++ android/app/src/main/java/com/mapuinblue/bunnmobileapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mapuinblue.bunnmobileapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'BunnMobileApp',
+  appId: 'com.mapuinblue.bunnmobileapp',
+  appName: 'Bunn Productivity Mobile',
   webDir: 'dist'
 };
 
